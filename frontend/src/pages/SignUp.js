fdf5412f8d0be907ded62b8e34611fc75b4ed499
PunithAchintha2003 <punithachintha@gmail.com>
2024-08-10 12:07:53 +0530

    Initial Commit 4 - SignUp (F,B)

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react'
 import loginIcons from '../assest/signin.gif'
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import { Link , useNavigate } from 'react-router-dom';
 import imageTobase64 from '../helpers/imageTobase64';
+import SummaryApi from '../common';
+import { toast } from 'react-toastify';
 
 const SignUp = () => {
 
-  const [showPassword,setShowPassword] = useState(false)
-  const [showConfirmPassword,setShowConfirmPassword] = useState(false)
+  const [showPassword,setShowPassword] = useState(false);
+  const [showConfirmPassword,setShowConfirmPassword] = useState(false);
 
     const [data,setData] = useState({
         email : "",
@@ -16,39 +18,62 @@ const SignUp = () => {
         name : "",
         confirmPassword : "",
         profilePic : "",
-    })
+    });
+    const navigate = useNavigate()
 
     const handleOnChange = (e) =>{
-        const {name,value} = e.target
+        const {name,value} = e.target;
 
         setData((preve)=>{
             return{
                 ...preve,
                 [name] : value
             }
-        })
-    }
+        });
+    };
 
     const handleUploadPic = async(e) => {
-        const file = e.target.files[0]
-        const imagePic = await imageTobase64(file)
+        const file = e.target.files[0];
+        const imagePic = await imageTobase64(file);
         setData((preve)=>{
             return{
               ...preve,
               profilePic : imagePic
             }
-        })
-    }
+        });
+    };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-    }
+    const handleSubmit = async(e) => {
+        e.preventDefault();
 
-    console.log("data login",data)
+        if(data.password === data.confirmPassword){
+
+            const dataResponse = await fetch(SummaryApi.signUp.url,{
+                method : SummaryApi.signUp.method,
+                headers : {
+                    "content-type" : "application/json"
+                },
+                body : JSON.stringify(data)
+            });
+            const dataApi = await dataResponse.json();
+
+            if(dataApi.success){
+                toast.success(dataApi.message)
+                navigate("/login")
+            }
+
+            if(dataApi.error){
+                toast.error(dataApi.message)
+            }
+
+        }else{
+            console.log("Please check password and confirm password");
+        }
+    };
 
   return (
     <section id='signup'>
-    <div className='mx-auto container p-4'>
+     <div className='mx-auto container p-4'>
         <div className='bg-white p-5 w-full max-w-sm mx-auto rounded'>
             <div className='w-20 h-20 mx-auto relative overflow-hidden rounded-full'>
                 <div>
@@ -64,7 +89,7 @@ const SignUp = () => {
                 </form>
             </div>
 
-            <from className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>
+            <form className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>
                  <div className='grid'>
                     <label>Name : </label>
                     <div className='bg-slate-100 p-2'>
@@ -146,10 +171,10 @@ const SignUp = () => {
                 </div>
 
                 <button className='bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-5'>Sign Up</button>
-            </from>
+            </form>
             <p className='my-5'>Already have account? <Link to={"/login"} className='text-red-600 hover:text-red-700 hover:underline'>Login</Link></p>
         </div>
-    </div>
+     </div>
     </section>
   )
 }

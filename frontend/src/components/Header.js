0673706d862f587107c17162c5a87c4d685c3727
PunithAchintha2003 <punithachintha@gmail.com>
2024-08-29 13:08:03 +0530

    Initial Commit 7 - Admin Panel ( All Users )

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from './Logo'
 import { GrSearch } from "react-icons/gr";
 import { FaRegCircleUser } from "react-icons/fa6";
@@ -8,13 +8,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import SummaryApi from '../common';
 import { toast } from 'react-toastify';
 import { setUserDetails } from '../store/userSlice';
+import ROLE from '../common/role';
 
 const Header = () => {
 
   const user = useSelector(state => state?.user?.user)
   const dispatch = useDispatch()
-
-  console.log("user header",user)
+  const [menuDisplay,setMenuDisplay] = useState(false)
 
   const handleLogout = async()=>{
     const fetchData = await fetch(SummaryApi.logout_user.url,{
@@ -52,14 +52,37 @@ const Header = () => {
         </div>
 
         <div className='flex items-center gap-7'>
-          <div className='text-3xl cursor-pointer'>
+
+          <div className='relative flex justify-center'>
+
             {
-              user?.profilePic ? (
-                <img src={user?.profilePic} className='w-10 h-10 rounded-full' alt={user?.name}/>
-              ) : (
-                <FaRegCircleUser/>
+              user?._id && (
+                <div className='text-3xl cursor-pointer relative flex justify-center' onClick={()=>setMenuDisplay(preve => !preve)}>
+                  {
+                   user?.profilePic ? (
+                     <img src={user?.profilePic} className='w-10 h-10 rounded-full' alt={user?.name}/>
+                   ) : (
+                     <FaRegCircleUser/>
+                   )
+                  }
+                </div>
               )
             }
+           
+           {
+            menuDisplay && (
+              <div className='absolute bg-white bottom-0 top-11 h-fit p-2 shadow-lg rounded'>
+                 <nav>
+                  {
+                    user?.role === ROLE.ADMIN && (
+                      <Link to={"/admin-panel/all-products"} className='whitespace-nowrap hidden md:block hover:bg-slate-100 p-2' onClick={()=>setMenuDisplay(preve => !preve)}>Admin Panel</Link>
+                    )
+                  }
+                 </nav>
+              </div>
+            )
+           }
+
           </div>
 
           <div className='text-3xl relative'>

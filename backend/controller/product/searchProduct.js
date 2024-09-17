aa262d5cac3515307e7516a757bda7673f81dfd1
PunithAchintha2003 <punithachintha@gmail.com>
2024-09-17 15:08:23 +0530

    Initial Commit 13 -Search

---
+++ backend/controller/product/searchProduct.js
@@ -0,0 +1,36 @@
+const productModule = require("../../models/productModel")
+
+const searchProduct = async(req,res)=>{
+    try{
+        const query = req.query.q
+
+        const regex = new RegExp(query,'i','g')
+
+        const product = await productModule.find({
+            "$or" : [
+                {
+                    productName : regex
+                },
+                {
+                    category : regex
+                }
+            ]
+        })
+
+        res.json({
+            data : product,
+            message : "Search Product List", 
+            error : false,
+            success : true
+        })
+
+    }catch(err){
+        res.json({
+            message : err.message || err, 
+            error : true,
+            success : false
+        })
+    }
+}
+
+module.exports = searchProduct
--- frontend/src/pages/ProductDetails.js
+++ frontend/src/pages/ProductDetails.js
@@ -51,7 +51,7 @@ const ProductDetails = () => {
 
   useEffect(()=>{
     fetchProductDetails()
-  },[])
+  },[params])
 
   const handeMouseEnterProduct = (imageURL)=>{
     setActiveImage(imageURL)
